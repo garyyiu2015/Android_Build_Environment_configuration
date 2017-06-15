c3649f1878c7e20de1fc13ee50a52949acd52a9d
garyyiu2015 <garyyiu2015@gmail.com>
2017-06-15 22:06:27 +0800

    data: 15th June 2017

---
+++ ubuntu1604linuxmint18.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+clear
+echo “安装编译环境需要的依赖库”
+sudo apt update -y
+sudo apt install bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk android-tools-adb android-tools-fastboot -y
+echo “依赖库安装完毕”
+echo “安装额外的工具”
+sudo apt install p7zip-full tree -y
+echo “工具安装完毕”
+
+if [ ! "$(which adb)" == "" ];
+then
+echo “配置adb”
+sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/snowdream/51-android/master/51-android.rules
+sudo chmod 644   /etc/udev/rules.d/51-android.rules
+sudo chown root /etc/udev/rules.d/51-android.rules
+sudo service udev restart
+adb kill-server
+sudo killall adb
+fi
+
+if [ ! "$(which fastboot)" == "/usr/bin/fastboot" ];
+then
+echo “修复fastboot权限”
+sudo chown root:root /usr/bin/fastboot
+sudo chmod +s /usr/bin/fastboot
+fi
+
+echo “安装repo”
+mkdir -p ~/bin
+curl https://raw.githubusercontent.com/akhilnarang/scripts/master/utils/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+
+echo “修复Linux Mint18.1下搜狗输入法的正常使用”
+sudo apt install fcitx-config-gtk fcitx-config-gtk2 fcitx-frontend-all fcitx-frontend-fbterm fcitx-libs-gclient fcitx-libs-qt5 fcitx-module-lua fcitx-tools fcitx-ui-light gir1.2-fcitx-1.0 -y
+
