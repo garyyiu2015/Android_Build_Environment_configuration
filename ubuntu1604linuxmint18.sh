76067de189af957b3913ba1ec405d327da3fea5f
gary <garyyiu2015@users.noreply.github.com>
2017-07-01 11:40:01 +0800

    Update ubuntu1604linuxmint18.sh
    编译cm-13.0需要

--- ubuntu1604linuxmint18.sh
+++ ubuntu1604linuxmint18.sh
@@ -5,7 +5,7 @@
 clear
 echo “安装编译环境需要的依赖库”
 sudo apt update -y
-sudo apt install bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk android-tools-adb android-tools-fastboot -y
+sudo apt install bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-devg lib32readline-dev lib32z1-dev libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev maven openjdk-8-jdk android-tools-adb android-tools-fastboot -y
 echo “依赖库安装完毕”
 echo “安装额外的工具”
 sudo apt install p7zip-full tree -y
